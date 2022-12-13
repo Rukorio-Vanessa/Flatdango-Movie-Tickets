16122144a9737ce6c72664471469858b0eedad6a
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-13 04:18:10 +0300

    updated html file

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     // create random meal element
-
-
-    
-    const getMovie = () => {
-        fetch("http://localhost:3000/films")
-            .then((response) => response.json())
-            .then((data) => {
-                const movieInfo = data.films[0]
-                const title = movieInfo.title
-                const description = movieInfo.description
-                const image = movieInfo.poster
-                const showtime = movieInfo.showtime
-                const runtime = movieInfo.runtime
-                const movieElement = createMovie(image, title, description, showtime, runtime)
-                document.querySelector('#movie').appendChild(movieElement)
-            })
+    function generateAnAnimal(movie){
+        let animalList = document.createElement('li')
+        animalList.className = 'list'
+        animalList.innerHTML = `
+          <img src="${animal.image}">
+          <div class="info">
+          <h4>${animal.id}</h4>
+          <h4>${animal.name}</h4>
+          <p>$<span class="vote-count">${animal.votes}</span> Voted </p>
+          </div>
+          <div class="button">
+            <button> VOTE </button>
+          </div>  
+          `
+    //add the animal list to DOM
+    document.getElementById("animal-list").appendChild(animalList)  
     }
-    getMovie()
-
-
 })
