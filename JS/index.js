3377dfb0e34d3fa678af466cfecf361af94838fb
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-13 04:02:08 +0300

    made GET request

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+    // create random meal element
+
+
+    
+    const getMovie = () => {
+        fetch("http://localhost:3000/films")
+            .then((response) => response.json())
+            .then((data) => {
+                const movieInfo = data.films[0]
+                const title = movieInfo.title
+                const description = movieInfo.description
+                const image = movieInfo.poster
+                const showtime = movieInfo.showtime
+                const runtime = movieInfo.runtime
+                const movieElement = createMovie(image, title, description, showtime, runtime)
+                document.querySelector('#movie').appendChild(movieElement)
+            })
+    }
+    getMovie()
+
+
+})
